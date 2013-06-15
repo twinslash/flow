c994237a7720a70378a3a67dfaba14c7871b45c2
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2013-06-15 17:10:24 +0300

    Small refactoring
    Use tabs instead of spaces
    Don't use new Flow if Flow already using
    Don't wrap an anonymous function

--- flow.js
+++ flow.js
@@ -1,67 +1,63 @@
-(function() {
+window.Flow = window.Flow || (function() {
 
-  window.Flow = (function() {
+	function Flow( slides, startPosition ) {
+		if ( typeof slides === "string" ) {
+			this.slides = [].slice.call( document.querySelectorAll( slides ) );
+		} else {
+			this.slides = slides;
+		}
 
-    function Flow( slides, startPosition ) {
-      if ( typeof slides === "string" ) {
-        this.slides = [].slice.call( document.querySelectorAll( slides ) );
-      } else {
-        this.slides = slides;
-      }
+		if ( typeof startPosition === "number" ) {
+			this.show( startPosition );
+		}
+	}
 
-      if ( typeof startPosition === "number" ) {
-        this.show( startPosition );
-      }
-    }
+	var FlowProto = Flow.prototype;
 
-    var FlowProto = Flow.prototype;
+	FlowProto.show = function( position ) {
+		if ( typeof position === "number" && !isNaN( position ) ) {
+			return this.movement( this.currentPosition = position );
+		} else {
+			throw TypeError("Flow.show( position ) - position isn't a number");
+		}
+	};
 
-    FlowProto.show = function( position ) {
-      if ( typeof position === "number" && !isNaN( position ) ) {
-        return this.movement( this.currentPosition = position );
-      } else {
-        throw TypeError("Flow.show( position ) - position isn't a number");
-      }
-    };
+	FlowProto.movement = function( position ) {
+		var l = this.slides.length,
+			i = 0;
 
-    FlowProto.movement = function( position ) {
-      var l = this.slides.length,
-        i = 0;
+		for ( ; i < l; i++ ) {
+			this.slides[ i ].style.display = "none";
+		}
 
-      for ( ; i < l; i++ ) {
-        this.slides[ i ].style.display = "none";
-      }
+		// @TODO: Need to set native display value instead of 'block'
+		this.slides[ position ].style.display = "block";
+	};
 
-      // @TODO: Need to set native display value instead of 'block'
-      this.slides[ position ].style.display = "block";
-    };
+	// Controllers
 
-    // Controllers
+	FlowProto.next = function() {
+		return this.show( this.currentPosition + 1 === this.size() ? 0 : this.currentPosition + 1 );
+	};
 
-    FlowProto.next = function() {
-      return this.show( this.currentPosition + 1 === this.size() ? 0 : this.currentPosition + 1 );
-    };
+	FlowProto.prev = function() {
+		return this.show( this.currentPosition === 0 ? this.size() - 1 : this.currentPosition - 1 );
+	};
 
-    FlowProto.prev = function() {
-      return this.show( this.currentPosition === 0 ? this.size() - 1 : this.currentPosition - 1 );
-    };
+	FlowProto.first = function() {
+		return this.show( 0 );
+	};
 
-    FlowProto.first = function() {
-      return this.show( 0 );
-    };
+	FlowProto.last = function() {
+		return this.show( this.size() - 1 );
+	};
 
-    FlowProto.last = function() {
-      return this.show( this.size() - 1 );
-    };
+	// Secondary
 
-    // Secondary
+	FlowProto.size = function() {
+		return this.slides.length;
+	};
 
-    FlowProto.size = function() {
-      return this.slides.length;
-    };
+	return Flow;
 
-    return Flow;
-
-  })();
-
-}).call( this );
+})();
